4e5e3cf1f2ad4253de4d4d2bfaec554b5abff607
Rosa Hernandez <rosacamarillo86@gmail.com>
2016-09-29 16:41:44 -0500

    Formato

--- integracionPOS/src/main/java/com/taurus/openbravo/integracionPOS/XML/impl/GT_CLS_ManagePriceXML.java
+++ integracionPOS/src/main/java/com/taurus/openbravo/integracionPOS/XML/impl/GT_CLS_ManagePriceXML.java
@@ -65,8 +65,8 @@ public class GT_CLS_ManagePriceXML extends GT_CLS_Base implements GT_CLS_XMLServ
 			}
 			System.out.println("");
 			precio = validarDatos(precio);
-			if(precio!=null){
-				xmlTemp.append(generarXML(precio,""));
+			if (precio != null) {
+				xmlTemp.append(generarXML(precio, ""));
 			}
 		} // for-principal
 		if (xmlTemp.length() > 0) {
@@ -75,46 +75,47 @@ public class GT_CLS_ManagePriceXML extends GT_CLS_Base implements GT_CLS_XMLServ
 		}
 	}
 
-	public GT_CLS_Precio validarDatos(GT_CLS_Precio precio) throws IOException{
+	public GT_CLS_Precio validarDatos(GT_CLS_Precio precio) throws IOException {
 		// Buscar product por clave principal. Ojo:, en OB las claves
-				// principales tendran un sufijo con la uom 
-				String searchKey = precio.getClavePrincipal() + "_" + precio.getUom();
-				String idProducto = buscar("Product", "searchKey", searchKey);
-			if(idProducto!=null){
-				precio.setArticulo(idProducto);
-				return precio;
-			} else {
-				List<String> mensaje = new ArrayList<>();
-				mensaje.add(GT_CLS_ReadProperties.getPropertieValue("mensaje.precio.noinsertado.producto") + " " + searchKey );
-				mensaje.add(GT_CLS_ReadProperties.getPropertieValue("mensaje.precio.noinsertado.elementosrelacionados"));
-				GT_CLS_SaveLog.guardarArchivo(mensaje);
-				GT_CLS_SaveLog.guardarLogDb(mensaje);
-			}
-			return null;
-		
+		// principales tendran un sufijo con la uom
+		String searchKey = precio.getClavePrincipal() + "_" + precio.getUom();
+		String idProducto = buscar("Product", "searchKey", searchKey);
+		if (idProducto != null) {
+			precio.setArticulo(idProducto);
+			return precio;
+		} else {
+			List<String> mensaje = new ArrayList<>();
+			mensaje.add(
+					GT_CLS_ReadProperties.getPropertieValue("mensaje.precio.noinsertado.producto") + " " + searchKey);
+			mensaje.add(GT_CLS_ReadProperties.getPropertieValue("mensaje.precio.noinsertado.elementosrelacionados"));
+			GT_CLS_SaveLog.guardarArchivo(mensaje);
+			GT_CLS_SaveLog.guardarLogDb(mensaje);
+		}
+		return null;
+
 	}
-	
+
 	public String generarXML(GT_CLS_Precio precio, String tipo) {
 		// Buscar priceList por canal
 		String nombreCanal = precio.getDestino() + "_" + precio.getCanal();
 		// 1. Obtener priceList_id por ese canal (name)
-		String idPriceList = buscar("PricingPriceList","name", nombreCanal);
+		String idPriceList = buscar("PricingPriceList", "name", nombreCanal);
 		String idVersion = buscar("PricingPriceListVersion", "pricelist.id", idPriceList);
 		// 2. id de la version
 
-			StringBuilder sb = new StringBuilder();
-			sb.append(addTag("priceListVersion", null, "id", idVersion, true));//"CED844EDFA054C288CC21EF08EBC8A56"
-			sb.append(addTag("product", null, "id", precio.getArticulo(), true));
-			sb.append(addTag("active", "true", null, null, false));
-			sb.append(addTag("listPrice", precio.getPrecioBase(), null, null, false));
-			sb.append(addTag("standardPrice", precio.getPrecioBase(), null, null, false));
-			String xmlTemp = sb.toString();
-			sb = new StringBuilder();
-			sb.append(addTag("PricingProductPrice", xmlTemp, null, null, false));
-			xmlTemp = sb.toString();
-			sb = new StringBuilder();
-			sb.append(addHeader()).append(xmlTemp).append(addFooter());
-			return sb.toString();
-		
+		StringBuilder sb = new StringBuilder();
+		sb.append(addTag("priceListVersion", null, "id", idVersion, true));// "CED844EDFA054C288CC21EF08EBC8A56"
+		sb.append(addTag("product", null, "id", precio.getArticulo(), true));
+		sb.append(addTag("active", "true", null, null, false));
+		sb.append(addTag("listPrice", precio.getPrecioBase(), null, null, false));
+		sb.append(addTag("standardPrice", precio.getPrecioBase(), null, null, false));
+		String xmlTemp = sb.toString();
+		sb = new StringBuilder();
+		sb.append(addTag("PricingProductPrice", xmlTemp, null, null, false));
+		xmlTemp = sb.toString();
+		sb = new StringBuilder();
+		sb.append(addHeader()).append(xmlTemp).append(addFooter());
+		return sb.toString();
+
 	}
 }
